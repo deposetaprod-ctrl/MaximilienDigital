27125faa9842ae81a8e5b5fbdd4dcbbacdf4613c
deposetaprod-ctrl <66799758+deposetaprod-ctrl@users.noreply.github.com>
2026-03-13 20:02:42 +0100

    fix: revert to next/script for google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { Navigation } from "@/components/Navigation";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,21 +35,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" className="dark" suppressHydrationWarning>
-      <head>
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RZL7R8MYVS"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-RZL7R8MYVS');
-            `,
-          }}
-        />
-      </head>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-RZL7R8MYVS"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RZL7R8MYVS');
+          `,
+        }}
+      />
       <body
         className={`${geistSans.variable} font-sans antialiased`}
       >
