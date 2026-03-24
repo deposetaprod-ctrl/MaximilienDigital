cf5e026f635c025267ec0d4b944b2d9db669838c
deposetaprod-ctrl <66799758+deposetaprod-ctrl@users.noreply.github.com>
2026-03-24 12:44:13 +0100

    ok

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://digimax.fr'
+  const baseUrl = 'https://digimax.agency'
   
   return [
     {
