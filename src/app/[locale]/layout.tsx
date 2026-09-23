aeae1f474b78d90ea1868f84ed19eb531fbb6834
deposetaprod-ctrl <66799758+deposetaprod-ctrl@users.noreply.github.com>
2026-09-23 03:34:23 +0200

    ok

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -188,6 +188,21 @@ export default async function RootLayout(
           `,
         }}
       />
+      {process.env.NEXT_PUBLIC_CLARITY_ID && (
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+            `,
+          }}
+        />
+      )}
       <body
         className={`${geistSans.variable} font-sans antialiased`}
       >
