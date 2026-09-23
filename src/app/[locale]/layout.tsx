fb5d0d999829035ef9bf61fd8451c1befd6fde14
deposetaprod-ctrl <66799758+deposetaprod-ctrl@users.noreply.github.com>
2026-09-24 00:06:10 +0200

    ok

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -96,12 +96,12 @@ export async function generateMetadata(props: { params: Promise<{ locale: string
     manifest: "/manifest.json",
     icons: {
       icon: [
-        { url: "/new_max.png" },
-        { url: "/new_max.png", sizes: "32x32", type: "image/png" },
-        { url: "/new_max.png", sizes: "192x192", type: "image/png" },
+        { url: "/new_max_rounded.png" },
+        { url: "/new_max_rounded.png", sizes: "32x32", type: "image/png" },
+        { url: "/new_max_rounded.png", sizes: "192x192", type: "image/png" },
       ],
       apple: [
-        { url: "/new_max.png" },
+        { url: "/new_max_rounded.png" },
       ],
     },
   };
