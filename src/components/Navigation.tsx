cf2c8d9bd519c1eea47d3ead37cab854785c194e
deposetaprod-ctrl <66799758+deposetaprod-ctrl@users.noreply.github.com>
2026-09-23 23:46:26 +0200

    ok

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -46,7 +46,7 @@ export function Navigation() {
           
           {/* LOGO */}
           <Link href="/" className="brand" onClick={() => trackClick("nav_logo")} style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-            <img src="/max.png" alt="Maximilien" style={{ width: '32px', height: '32px', borderRadius: '50%', objectFit: 'cover', border: '1px solid #46362c' }} />
+            <img src="/new_max.png" alt="Maximilien" style={{ width: '32px', height: '32px', borderRadius: '50%', objectFit: 'cover', border: '1px solid #46362c' }} />
             <div>maximilien<span>.digital</span></div>
           </Link>
 
